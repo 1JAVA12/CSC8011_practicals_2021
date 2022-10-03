9240ccd8cc41edd8faf3361c3b966e5f25155be1
c2069035 <s.jagannathan2@ncl.ac.uk>
2022-10-03 15:15:12 +0100

    initial commit

--- easy/practical-1/HelloWorld.java
+++ easy/practical-1/HelloWorld.java
@@ -2,13 +2,17 @@ public class HelloWorld {
     
     public static void main(String[] args) {
 
-
-
+        System.out.println("Valli");
+        System.out.println("Introduction to Software Engineering");
+        System.out.println("Hello World");
         addition(); //ignore for now
         fancyPrint(); // ignore for now
     }
     
     public static void addition() {
+        int x=50;
+        int y=100;
+        int z=x+y;
 
     }
     
